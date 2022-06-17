775683b60f55a43385cb5c1152cf7bea75349dbc
viniciussec <viniciussec@hotmail.com>
2022-06-17 18:25:56 -0300

    fix: Corrigindo estilização do header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Gestor de campeonatos</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,13 @@
+import Head from "next/head";
+
 export default function Index() {
-  return <div className="text-red-500">Bem vindo</div>;
+  return (
+    <div>
+      <Head>
+        <title>Gestor de campeonatos</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <div className="text-red-500">Bem vindo</div>
+    </div>
+  );
 }
