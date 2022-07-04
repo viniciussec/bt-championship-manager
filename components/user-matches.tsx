e96f2d544153664e6e14ed12457532cecf4350be
Jefferson <jeffersonteixejunior@gmail.com>
2022-07-04 18:24:51 -0300

    perfil de usuario

---
+++ components/user-matches.tsx
@@ -0,0 +1,62 @@
+export default function UserMatches() {
+    return (
+        <>
+            <p className="font-semibold">Historico de Partidas</p>
+            <div className="flex flex-col justify-center w-1/2 p-4 mt-4 bg-white rounded-md">
+                <div className="flex flex-col items-center ">
+                    <div className="bg-[#6EA8F7] w-full mt-1 rounded-md">
+                        <div className="flex justify-between p-4 text-white border-b-2 font-bold">
+                            <p className="semi-bold">Equipe 1</p>
+                            <p>6</p>
+                        </div>
+                        <div className="flex justify-between p-4 text-white">
+                            <p>Equipe 2</p>
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div className="bg-[#6EA8F7] w-full mt-1 rounded-md">
+                        <div className="flex justify-between p-4 text-white border-b-2 font-bold">
+                            <p>Equipe 1</p>
+                            <p>6</p>
+                        </div>
+                        <div className="flex justify-between p-4 text-white">
+                            <p>Equipe 2</p>
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div className="bg-[#6EA8F7] w-full mt-1 rounded-md">
+                        <div className="flex justify-between p-4 text-white border-b-2 font-bold">
+                            <p className="semi-bold">Equipe 1</p>
+                            <p>6</p>
+                        </div>
+                        <div className="flex justify-between p-4 text-white">
+                            <p>Equipe 2</p>
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div className="bg-[#6EA8F7] w-full mt-1 rounded-md">
+                        <div className="flex justify-between p-4 text-white border-b-2 font-bold">
+                            <p className="semi-bold">Equipe 1</p>
+                            <p>6</p>
+                        </div>
+                        <div className="flex justify-between p-4 text-white">
+                            <p>Equipe 2</p>
+                            <p>1</p>
+                        </div>
+                    </div>
+                    <div className="bg-[#6EA8F7] w-full mt-1 rounded-md">
+                        <div className="flex justify-between p-4 text-white border-b-2 font-bold">
+                            <p className="semi-bold">Equipe 1</p>
+                            <p>6</p>
+                        </div>
+                        <div className="flex justify-between p-4 text-white">
+                            <p>Equipe 2</p>
+                            <p>1</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
