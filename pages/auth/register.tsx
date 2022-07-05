13815cae72a3a55a42302867b0983ee56b122d68
dytron <guilhermealfaiagui1@gmail.com>
2022-07-05 13:44:28 -0300

    Correção do tipo de conta na página de cadastro

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -11,7 +11,7 @@ export default  function Register(){
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [type, setType] = useState("");
+  const [type, setType] = useState("spectator");
   const [message, setMessage] = useState("");
   const [messageType, setMessageType] = useState("");
 
